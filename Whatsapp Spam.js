d2d07fd47c3b09c1a7fe50ed408029bd59472e8f
Patrick <trick_stival@hotmail.com>
2018-08-05 02:04:14 -0300

    Update Whatsapp Spam.js

--- Whatsapp Spam.js
+++ Whatsapp Spam.js
@@ -3,7 +3,7 @@ Utilização: colar no console do chrome.
 Passe como parâmetros da função a mensagem e a quantidade de vezes que quer mandá-la.
 */
 
-((mensagem = 'Mensagem de teste', quantidadeDeVezes = 1) => {
+((mensagem, quantidadeDeVezes) => {
   const caixaTexto = document.querySelector('._2S1VP')
 
   for (let i = 0; i < quantidadeDeVezes; i++) {
@@ -15,7 +15,7 @@ Passe como parâmetros da função a mensagem e a quantidade de vezes que quer m
     botao.click()
   }
 
-})()
+})('Mensagem de teste', 1)
 
 // Versão minificada:
 ((a,b)=>{var c=document.querySelector('._2S1VP');for(var d=0;d<b;d++)c.innerHTML=a,c.dispatchEvent(new Event('input',{bubbles:!0})),document.querySelector('._35EW6').click()})('mensagem', 2);
