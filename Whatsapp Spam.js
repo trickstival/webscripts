e3294553f3eb5f72cb5c6da76bd90759f39a891d
Patrick <trick_stival@hotmail.com>
2018-08-05 15:40:08 -0300

    Update Whatsapp Spam.js

--- Whatsapp Spam.js
+++ Whatsapp Spam.js
@@ -19,3 +19,23 @@ Passe como parâmetros da função a mensagem e a quantidade de vezes que quer m
 
 // Versão minificada:
 ((a,b)=>{var c=document.querySelector('._2S1VP');for(var d=0;d<b;d++)c.innerHTML=a,c.dispatchEvent(new Event('input',{bubbles:!0})),document.querySelector('._35EW6').click()})('mensagem', 2);
+
+// Versão com timeout:
+(async (mensagem = 'Mensagem de teste', quantidadeDeVezes = 1) => {
+  const caixaTexto = document.querySelector('._2S1VP')
+
+  function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  for (let i = 0;i<quantidadeDeVezes;i++) {
+      await timeout(5000)
+      caixaTexto.innerHTML = mensagem + ' ' + i
+
+      caixaTexto.dispatchEvent(new Event('input', { bubbles: true }))
+    
+      const botao = document.querySelector('._35EW6')
+      botao.click()
+  }
+
+})('oi', 8)
